eff3bd2cc43d7719ec6dfdc9870f87a65146cf7a
gyesswhat <h2weirdo@icloud.com>
2025-07-20 00:34:58 +0900

    fix: 첫 어시스턴트 메시지 안 보이는 에러 수정

---
+++ src/components/TypingIndicator.tsx
@@ -0,0 +1,14 @@
+// src/components/TypingIndicator.tsx
+import "./TypingIndicator.css";
+
+function TypingIndicator() {
+  return (
+    <div className="typing-indicator-bubble">
+      <span className="dot" />
+      <span className="dot" />
+      <span className="dot" />
+    </div>
+  );
+}
+
+export default TypingIndicator;
